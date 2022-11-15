7fa4b9affdd7ac678d362694a55760b0c1071495
tatialveso <tatiialveso@gmail.com>
2022-11-15 14:24:27 -0300

    :sparkles: iteration 1

---
+++ src/HomePage/HomePage.js
@@ -0,0 +1,46 @@
+import { Card, Col, Container, Row } from "react-bootstrap"
+import { Link } from "react-router-dom"
+import allBeersImg from '../assets/beers.png'
+import newBeerImg from '../assets/new-beer.png'
+import randomBeerImg from '../assets/random-beer.png'
+
+function HomePage() {
+    return (
+        <Container className="h-100">
+            <Row className="align-items-center h-100">
+                <Col>
+                    <Card>
+                        <Card.Img variant="top" src={allBeersImg} />
+                        <Card.Body>
+                            <Card.Title>
+                                <Link className="nav-link" to="/beers">All Beers</Link>
+                            </Card.Title>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card>
+                        <Card.Img variant="top" src={randomBeerImg} />
+                        <Card.Body>
+                            <Card.Title>
+                                <Link className="nav-link" to="/random-beer">Random Beer</Link>
+                            </Card.Title>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card>
+                        <Card.Img variant="top" src={newBeerImg} />
+                        <Card.Body>
+                            <Card.Title>
+                                <Link className="nav-link" to="/new-beer">New Beer</Link>
+                            </Card.Title>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default HomePage
